3ef20943ac4fa638921f13ef398aa2715c0f42ee
joseph12896 <joseph12896@yahoo.com.tw>
2019-01-14 01:38:20 +0800

    Initial commit

---
+++ src/main/kotlin/Chu/com/Hello.kt
@@ -0,0 +1,14 @@
+package Chu.com
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+        println("Hello kotlin!")
+    }
+}
